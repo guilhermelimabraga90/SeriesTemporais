5d45a531043d63523a483ed904ad0e2752782bf9
Guilherme Lima Braga <guilhermelimabraga90@gmail.com>
2024-05-11 19:35:37 -0300

    Git para Sereis Temporais

---
+++ src/app/LSTM.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import SimpleRNN
+from keras.layers import Dense, Dropout, LSTM, GRU, Bidirectional
+from keras.optimizers import SGD
+import tensorflow as tf
+import os
+from tensorflow.keras import layers
+from tensorflow.keras.optimizers import Adam
+
+data = pd.read_csv('scr/csv/all_stocks_5yr.csv')
+
+df = data[data['Name'] == "AAL"]
+
+def plot_predictions(test, predicted):
+    plt.plot(test, color="red", label="real stock price")
+    plt.plot(predicted, color="blue", label="predicted stock price")
+    plt.title("Stock price prediction")
+    plt.xlabel("time")
+    plt.ylabel("Stock price")
+    plt.legend()
+    plt.show()
+
+df = data[data['Name'] == "AAL"]
+
+df['date'] = pd.to_datetime(df['date'])
+
+df = df.set_index('date')
+
+df.head()
+
+train = df[:'2016'].iloc[:,1:2].values
+test = df['2017':].iloc[:,1:2].values
+
+df["close"][:'2016'].plot(figsize=(16,4), legend=True)
+df["close"]["2017":].plot(figsize=(16,4), legend=True)
+plt.legend(["Training set (before 2017)", "Test set (from 2017)"])
+plt.title("Stock prices")
+plt.show()
+
+sc = MinMaxScaler(feature_range=(0,1))
+train_scaled = sc.fit_transform(train)
+
+x_train = []
+y_train = []
+
+for i in range(60,982 ):
+    x_train.append(train_scaled[i-60:i, 0])
+    y_train.append(train_scaled[i,0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+dataset_total = pd.concat((df['close'][:'2016'], df['close']['2017':]), axis=0)
+print(dataset_total.shape)
+
+inputs = dataset_total[len(dataset_total)-len(test)-60 : ].values
+print(inputs.shape)
+inputs = inputs.reshape(-1,1)
+print(inputs.shape)
+inputs = sc.transform(inputs)
+print(inputs.shape)
+
+x_test = []
+for i in range(60,311):
+    x_test.append(inputs[i-60:i, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+regressor = Sequential()
+
+regressor.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1],1)))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units=50, return_sequences=True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units=50, return_sequences=True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units=50))
+regressor.add(Dropout(0.2))
+
+regressor.add(Dense(units=1))
+
+regressor.compile(optimizer='rmsprop', loss='mean_squared_error')
+
+regressor.fit(x_train, y_train, epochs=50, batch_size=32)
+
+dataset_total = pd.concat((df['close'][:'2016'], df['close']['2017':]), axis=0)
+print(dataset_total.shape)
+
+inputs = dataset_total[len(dataset_total)-len(test)-60 : ].values
+print(inputs.shape)
+inputs = inputs.reshape(-1,1)
+print(inputs.shape)
+inputs = sc.transform(inputs)
+print(inputs.shape)
+
+x_test = []
+for i in range(60,311):
+    x_test.append(inputs[i-60:i, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+predicted = regressor.predict(x_test)
+predicted = sc.inverse_transform(predicted)
+
+plot_predictions(test, predicted)
---
+++ src/app/Rnn.py
@@ -0,0 +1,130 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import SimpleRNN
+from keras.layers import Dense, Dropout, LSTM, GRU, Bidirectional
+from keras.optimizers import SGD
+import tensorflow as tf
+import os
+from tensorflow.keras import layers
+from tensorflow.keras.optimizers import Adam
+
+data = pd.read_csv('scr/csv/all_stocks_5yr.csv')
+
+df = data[data['Name'] == "AAL"]
+
+train_length = round(len(df)*0.7)
+lg = len(df)
+val_length = lg-train_length
+
+train_data = df['close'][:train_length,]
+val_data = df['close'][train_length:,]
+
+train=train_data.values.reshape(-1,1)
+
+scaler = MinMaxScaler()
+scaled_trainset = scaler.fit_transform(train)
+
+x_train = []
+y_train = []
+step = 50
+
+for i in range(step, train_length):
+    x_train.append(scaled_trainset[i-step:i,0])
+    y_train.append(scaled_trainset[i,0])
+
+X_train, y_train = np.array(x_train), np.array(y_train)
+
+X_train = np.reshape(X_train, (X_train.shape[0],X_train.shape[1],1))
+y_train.reshape(y_train.shape[0],1)
+
+model = Sequential()
+
+model.add(
+    SimpleRNN(units = 50,return_sequences= True,input_shape = (X_train.shape[1],1)))
+
+model.add(
+    Dropout(0.2))
+
+model.add(
+    SimpleRNN(units = 50, return_sequences = True)
+             )
+
+model.add(
+    Dropout(0.2)
+             )
+
+model.add(
+    SimpleRNN(units = 50, return_sequences = True)
+             )
+
+model.add(
+    Dropout(0.2)
+             )
+
+model.add(
+    SimpleRNN(units = 50)
+             )
+
+model.add(
+    Dropout(0.2)
+             )
+
+model.add(
+    Dense(units = 1))
+
+model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = ['accuracy'])
+
+history = model.fit(X_train, y_train, epochs = 50, batch_size =32)
+
+y_pred = model.predict(X_train)
+y_pred = scaler.inverse_transform(y_pred.reshape(1,-1))
+
+y_train = scaler.inverse_transform(y_train.reshape(1,-1))
+y_train
+
+y_train.shape
+y_train = np.reshape(y_train, (831 ,1))
+
+y_pred.shape
+y_pred = np.reshape(y_pred,(831 ,1))
+
+plt.figure(figsize = (30,10))
+plt.plot(y_pred, ls = '--', label = 'y_pred', lw = 2)
+plt.plot(y_train, label = 'y_train')
+plt.legend()
+plt.show()
+
+val = val_data.values.reshape(-1,1)
+
+scaled_valset = scaler.fit_transform(val)
+
+xval_train = []
+yval_train = []
+step = 50
+
+for i in range(step, val_length):
+    xval_train.append(scaled_valset[i-step:i,0])
+    yval_train.append(scaled_valset[i,0])
+
+X_val, y_val = np.array(xval_train), np.array(yval_train)
+
+X_val = np.reshape(X_val, (X_val.shape[0],X_val.shape[1],1))
+y_val = np.reshape(y_val, (-1,1))
+
+y_pred_val = model.predict(X_val)
+
+y_pred_val = scaler.inverse_transform(y_pred_val)
+
+y_val_is = scaler.inverse_transform(y_val)
+
+plt.figure(figsize = (30,10))
+plt.plot(y_pred_val, label = 'y_pred')
+plt.plot(y_val_is, label = 'y_val')
+plt.legend()
+plt.show()
